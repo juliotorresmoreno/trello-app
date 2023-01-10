02acaf999b9ef05ace32ff10dc6a8c27bb3f5838
Julio Torres <jtorres990@gmail.com>
2023-01-10 18:45:26 -0500

    feat(preload): the preconfiguration of the board is carried out

--- internal/app/controllers/trello.go
+++ internal/app/controllers/trello.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
+	"strings"
 
-	"github.com/juliotorresmoreno/trello-app/internal/app/services"
+	services "github.com/juliotorresmoreno/trello-app/internal/app/services/trello-service"
+	trello_service "github.com/juliotorresmoreno/trello-app/internal/app/services/trello-service"
 	"github.com/labstack/echo/v4"
 )
 
@@ -27,26 +27,47 @@ type createSchema struct {
 	Category    string `json:"category"`
 }
 
-var trelloService = services.NewTrelloService()
+var trelloService = trello_service.NewTrelloService()
 
+// getLabels: i could not find how can i create new cards with types.
+func getLabels(labels trello_service.Labels, Type string, Category string) []string {
+	list := make([]string, 0)
+	if label, ok := labels[Type]; ok {
+		list = append(list, label.Id)
+	}
+	if Type == "task" && Category == "Maintenance" {
+		category := strings.ToLower(Category)
+		if label, ok := labels[category]; ok {
+			list = append(list, label.Id)
+		}
+	}
+	return list
+}
 func (t trelloApi) create(c echo.Context) error {
 	var err error
 	payload := new(createSchema)
 	c.Bind(payload)
 	response := map[string]interface{}{}
 
+	Alllabels, err := trelloService.GetLabels()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Internal Server Error",
+		})
+	}
+	labels := getLabels(Alllabels, payload.Type, payload.Category)
+
 	data := services.CreateCardScheme{
-		Name: payload.Title,
-		Desc: payload.Description,
+		Name:     payload.Title,
+		Desc:     payload.Description,
+		IdLabels: labels,
 	}
 	err = trelloService.CreateCard(data)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message": err.Error(),
+			"message": "Internal Server Error",
 		})
 	}
-
-	json.NewEncoder(os.Stdout).Encode(payload)
 
 	// It's ideal that you do not send answer because you are send status code 201.
 	return c.JSON(http.StatusCreated, response)
--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/juliotorresmoreno/trello-app/configs"
 	"github.com/juliotorresmoreno/trello-app/internal/app"
+	"github.com/juliotorresmoreno/trello-app/internal/app/preload"
 )
 
 func main() {
 	e := app.NewServer()
 
 	conf := configs.GetConfig()
+
+	preload.TrelloPreload()
 
 	addr := fmt.Sprintf(":%v", conf.Port)
 	e.Logger.Fatal(e.Start(addr))
