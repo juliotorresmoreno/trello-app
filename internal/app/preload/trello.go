e2f3388180460723e029b59c2d31334be4b043a3
Julio Torres <jtorres990@gmail.com>
2023-01-11 01:42:46 -0500

    feat(docs): some comments are added

--- internal/app/preload/trello.go
+++ internal/app/preload/trello.go
@@ -2,6 +2,8 @@ package preload
 
 import trello_service "github.com/juliotorresmoreno/trello-app/internal/app/services/trello-service"
 
+// This works as a kind of database migration but it's not really.
+// Prepare the labels on the board that will be used.
 func TrelloPreload() {
 	var trelloService = trello_service.NewTrelloService()
 
