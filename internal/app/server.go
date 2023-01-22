1499331198d42a5946e2b79b5a3fb04ac1bf786f
Julio Torres <jtorres990@gmail.com>
2023-01-22 16:35:44 -0500

    feat(secure): add security police and prometeus

--- internal/app/server.go
+++ internal/app/server.go
@@ -2,10 +2,13 @@ package app
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/juliotorresmoreno/trello-app/configs"
 
 	"github.com/juliotorresmoreno/trello-app/internal/app/controllers"
+	"github.com/juliotorresmoreno/trello-app/internal/app/middlewares"
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -21,8 +24,28 @@ func NewServer() *echo.Echo {
 	if conf.Env != "production" {
 		e.Use(middleware.Logger())
 	}
+
+	sources := []string{
+		"default-src 'self' unpkg.com fonts.googleapis.com",
+		"script-src 'self' unpkg.com 'sha256-Y6uMcHW6QmLl5jRFbZ/tRzmQ0a0EjDOkQO0vpHPjiRE='",
+		"img-src 'self' data:",
+	}
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{
+		XSSProtection:         "",
+		ContentTypeNosniff:    "",
+		XFrameOptions:         "",
+		HSTSMaxAge:            3600,
+		ContentSecurityPolicy: strings.Join(sources, "; "),
+		Skipper: func(c echo.Context) bool {
+			return c.Request().URL.Path != "/api/v1/docs"
+		},
+	}))
+	p := prometheus.NewPrometheus("echo", nil)
+	p.Use(e)
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
+	gzip := middleware.GzipWithConfig(middleware.DefaultGzipConfig)
+	e.Use(middlewares.Ommit(gzip, []string{"/metrics"}))
 
 	e.GET("/", HealthCheck)
 
